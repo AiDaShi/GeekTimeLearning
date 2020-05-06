2acef07cd4deca5d853654798be5a2afede01bf6
AiDaShi <86382516@qq.com>
2020-05-06 15:39:15 +0800

    commit DependencyInjectionDemo

---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjectionDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DependencyInjectionDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetService")]
+        public int GetService([FromServices]IMySingletonService singleton1,
+                              [FromServices]IMySingletonService singleton2,
+                              [FromServices]IMyTransientService transient1,
+                              [FromServices]IMyTransientService transient2,
+                              [FromServices]IMyScopedService scoped1,
+                              [FromServices]IMyScopedService scoped2)
+        {
+            Console.WriteLine($"singleton1:{singleton1.GetHashCode()}");
+            Console.WriteLine($"singleton2:{singleton2.GetHashCode()}");
+
+
+            Console.WriteLine($"transient1:{transient1.GetHashCode()}");
+            Console.WriteLine($"transient2:{transient2.GetHashCode()}");
+
+            Console.WriteLine($"scoped1:{scoped1.GetHashCode()}");
+            Console.WriteLine($"scoped2:{scoped2.GetHashCode()}");
+
+            Console.WriteLine($"========GetService 请求结束=======");
+            return 1;
+        }
+
+        [HttpGet("GetService2")]
+        public int GetService2([FromServices]IMySingletonService singleton1,
+                              [FromServices]IMySingletonService singleton2,
+                              [FromServices]IMyTransientService transient1,
+                              [FromServices]IMyTransientService transient2,
+                              [FromServices]IMyScopedService scoped1,
+                              [FromServices]IMyScopedService scoped2)
+        {
+            Console.WriteLine($"singleton1:{singleton1.GetHashCode()}");
+            Console.WriteLine($"singleton2:{singleton2.GetHashCode()}");
+
+
+            Console.WriteLine($"transient1:{transient1.GetHashCode()}");
+            Console.WriteLine($"transient2:{transient2.GetHashCode()}");
+
+            Console.WriteLine($"scoped1:{scoped1.GetHashCode()}");
+            Console.WriteLine($"scoped2:{scoped2.GetHashCode()}");
+
+            Console.WriteLine($"========GetService 2 请求结束=======");
+            return 1;
+        }
+
+
+        /// <summary>
+        /// 获取 Service
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        [HttpGet("GetServiceList")]
+        public int GetServiceList([FromServices]IEnumerable<IOrderService> services)
+        {
+            foreach (var item in services)
+            {
+                Console.WriteLine($"Get Servic instances：{item.ToString()}:{item.GetHashCode()}");
+            }
+            return 1;
+        }
+    }
+}
--- StarupDemo/StarupDemo/Program.cs
+++ StarupDemo/StarupDemo/Program.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace StarupDemo
@@ -22,6 +24,22 @@ namespace StarupDemo
                 {
                     Console.WriteLine("Configure WebHost Defaults");
                     webBuilder.UseStartup<Startup>();
+                    //webBuilder.ConfigureServices(services => {
+                    //    Console.WriteLine("webBuilder - Configure Services");
+                    //    services.AddControllers();
+                    //});
+                    //webBuilder.Configure(app => {
+                    //    Console.WriteLine("webBuilder - Configure");
+
+                    //    app.UseRouting();
+
+                    //    app.UseAuthorization();
+
+                    //    app.UseEndpoints(endpoints =>
+                    //    {
+                    //        endpoints.MapControllers();
+                    //    });
+                    //});
                 })
                 .ConfigureHostConfiguration(builder =>
                 {
